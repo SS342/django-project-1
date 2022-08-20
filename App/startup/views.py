009def00df3f6f2f2500b2f80a51d7574e57823b
SS342 <alex2005ov@gmail.com>
2022-08-20 19:26:31 +0300

    first commit

---
+++ App/startup/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import datetime
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = "Allelleo"
+
+    context = {
+        'date': date,
+        'name': name,
+    }
+
+    return render(request, 'home.html', context)
